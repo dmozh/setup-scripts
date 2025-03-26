87122ae79fc95597274973f1865c805688bf32c2
Danila Mozheyko <35331557+dmozh@users.noreply.github.com>
2025-03-26 20:43:09 +0300

    Update and rename download_repos.sh to download_img.sh

--- download_img.sh
+++ download_img.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+wget https://distro.ibiblio.org/archlinux/images/latest/Arch-Linux-x86_64-cloudimg.qcow2
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
