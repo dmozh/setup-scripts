467638a4ec42e853db52eca07d112edc7a6e4b15
Danila Mozheyko <35331557+dmozh@users.noreply.github.com>
2025-04-03 16:02:33 +0300

    Update download_img.sh

--- download_img.sh
+++ download_img.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://mirror.yandex.ru/archlinux/iso/2025.03.01/archlinux-x86_64.iso
+wget https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/aarch64/alpine-virt-3.21.3-aarch64.iso
+wget https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-virt-3.21.3-x86_64.iso
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
