3495dde17d8fa21f6a63e0536cca1655388b39cb
Danila Mozheyko <35331557+dmozh@users.noreply.github.com>
2025-04-04 12:00:42 +0300

    Create mirrors.sh

---
+++ alpine/mirrors.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+apk add wget
+
+ALPINE_VERSION="v$(cut -d. -f1 /etc/alpine-release).$(cut -d. -f2 /etc/alpine-release)"
+
+TEMP_FILE=$(mktemp)
+
+function clear {
+  rm $TEMP_FILE $0
+}
+
+trap clear EXIT
+
+wget ${MIRRORS_LIST:=""} -O $TEMP_FILE
+
+for mirror in $(head -${MIRRORS_COUNT:=5} $TEMP_FILE)
+do
+  echo "${mirror}${ALPINE_VERSION}/main" >> /etc/apk/repositories
+  echo "${mirror}${ALPINE_VERSION}/community" >> /etc/apk/repositories
+done
