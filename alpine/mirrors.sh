974b3b128d9a4271f1d5994703d411dd6c855791
Danila Mozheyko <35331557+dmozh@users.noreply.github.com>
2025-04-04 12:21:07 +0300

    Update mirrors.sh

--- alpine/mirrors.sh
+++ alpine/mirrors.sh
@@ -3,18 +3,12 @@
 ALPINE_VERSION="v$(cut -d. -f1 /etc/alpine-release).$(cut -d. -f2 /etc/alpine-release)"
 echo ${ALPINE_VERSION}
 
-# TEMP_FILE=$(mktemp)
+TEMP_FILE=$(mktemp)
 
-# function clear {
-#   rm $TEMP_FILE $0
-# }
+wget ${MIRRORS_LIST:="https://github.com/dmozh/setup-scripts/blob/master/alpine/MIRRORS.txt"} -O $TEMP_FILE
 
-# trap clear EXIT
-
-# wget ${MIRRORS_LIST:="https://github.com/dmozh/setup-scripts/blob/master/alpine/MIRRORS.txt"} -O $TEMP_FILE
-
-# for mirror in $(head -${MIRRORS_COUNT:=5} $TEMP_FILE)
-# do
-#   echo "${mirror}${ALPINE_VERSION}/main" >> /etc/apk/repositories
-#   echo "${mirror}${ALPINE_VERSION}/community" >> /etc/apk/repositories
-# done
+for mirror in $(head -${MIRRORS_COUNT:=5} $TEMP_FILE)
+do
+  echo "${mirror}${ALPINE_VERSION}/main" >> /etc/apk/repositories
+  echo "${mirror}${ALPINE_VERSION}/community" >> /etc/apk/repositories
+done
